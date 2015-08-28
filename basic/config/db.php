52c30e1af3d721869bc83133490fa8e310fd0047
tryit <amstudio777@gmail.com>
2015-08-28 17:15:42 +0300

    init

---
+++ basic/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=sakila',
+    'username' => 'root',
+    'password' => 'tryit',
+    'charset' => 'utf8',
+];
